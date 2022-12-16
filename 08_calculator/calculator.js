0670c5c3005c442a18a41b54d47147b32bdca81f
Evgeniya Khatulkova <hatulkova.ev@gmail.com>
2022-12-16 22:44:29 +0100

    Exercises finished.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function addition (total, currentValue) {
+  return total + currentValue;
 };
 
-const subtract = function() {
-	
+const subtract = function substraction (total, currentValue) {
+  return total - currentValue;
 };
 
-const sum = function() {
-	
+const sum = function(array1) {
+  const dodo = array1.reduce((total, currentValue) => total + currentValue, 0);
+  return dodo;
 };
 
-const multiply = function() {
-
+const multiply = function(array1) {
+  const multy = array1.reduce((total, currentValue) => total * currentValue);
+  return multy;
 };
 
-const power = function() {
-	
+const power = function(total, currentValue) {
+  return Math.pow(total, currentValue);
 };
 
-const factorial = function() {
-	
+const factorial = function(array1) { 
+  let total = 1;
+  if (array1 < 2) {
+    return 1;
+  } 
+  else {
+    while(array1 > 0){
+    total = total * array1;
+    array1--;}
+    return total;
+  }
 };
 
 // Do not edit below this line
