26cbef5ec14e749322a6b05b3a1c825ed11a079a
Evgeniya Khatulkova <hatulkova.ev@gmail.com>
2022-12-17 21:59:50 +0100

    Exercise is finished.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+strNew = str.replaceAll(/ /g,'')
+            .replaceAll(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,'')
+            .toLowerCase();
+const letters = Array.from(strNew);
+const newLetters = letters.map((x,i,a) => a[a.length-1-i]);
+const answerTo = newLetters.every((x,i,a) =>a[i] === letters[i]);
+return answerTo;
 };
 
 // Do not edit below this line
