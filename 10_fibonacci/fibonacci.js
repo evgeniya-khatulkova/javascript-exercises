be59e82513465a1cd999d7672251a2d5e9e729d7
Evgeniya Khatulkova <hatulkova.ev@gmail.com>
2022-12-18 19:00:14 +0100

    Exercise is finished.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,16 @@
-const fibonacci = function() {
-
-};
+const fibonacci = function(number) {
+if (number < 0 || number === NaN) {
+    return "OOPS"
+} else {
+    let start = 1;
+    let total = 0;
+    //let myNumber = number - 1;
+for (i = 0; i < number; i++) {
+    total = total + start; 
+    start = total - start;
+}
+return total;
+}};
 
 // Do not edit below this line
 module.exports = fibonacci;
