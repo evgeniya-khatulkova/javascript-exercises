a0e3db436f9b20de8a4e09b46021be7bb553d2de
Evgeniya Khatulkova <hatulkova.ev@gmail.com>
2022-12-19 15:43:28 +0100

    Exercise is finished.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,9 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+const currentDate = new Date;
+const useYear = currentDate.getFullYear();
 
+const findIt = people.sort((a, b) => ((a.yearOfDeath || useYear) - a.yearOfBirth) > ((b.yearOfDeath || useYear) - b.yearOfBirth) ? -1 : 1);
+return findIt[0];
 };
 
 // Do not edit below this line
